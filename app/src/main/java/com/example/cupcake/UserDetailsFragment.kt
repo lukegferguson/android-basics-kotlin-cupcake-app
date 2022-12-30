c9958c68c8724034588e1de64332a2a01e74c130
lukegferguson <34584109+lukegferguson@users.noreply.github.com>
2022-12-29 20:05:18 -0600

    Add User Details fragment, add to NavGraph, implement navigation buttons.

---
+++ app/src/main/java/com/example/cupcake/UserDetailsFragment.kt
@@ -0,0 +1,56 @@
+package com.example.cupcake
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.cupcake.databinding.FragmentUserdetailsBinding
+import com.example.cupcake.model.OrderViewModel
+
+/*
+* [UserDetailsFragment] allows the user to enter name and phone number
+* */
+class UserDetailsFragment: Fragment() {
+
+    private var binding: FragmentUserdetailsBinding? = null
+
+    private val sharedViewModel: OrderViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding = FragmentUserdetailsBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply{
+            viewModel = sharedViewModel
+            lifecycleOwner = viewLifecycleOwner
+            userDetailsFragment = this@UserDetailsFragment
+        }
+    }
+
+    fun goToNextScreen(){
+        findNavController().navigate(R.id.action_userDetailsFragment_to_summaryFragment)
+    }
+
+    fun cancelOrder(){
+        findNavController().navigate(R.id.action_userDetailsFragment_to_startFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+
+}
